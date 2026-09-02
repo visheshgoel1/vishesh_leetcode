3ce6dbd3aaffdcb526998cf169267311ebe406cf
Vishesh Goel <visheshgoel0007@gmail.com>
2026-09-02 11:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (79.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int f(int i, vector<int>& nums, vector<int>& dp){
-        if(i == 0) return nums[i];
-        if(i < 0) return 0;
-        if(dp[i] != -1) return dp[i];
-
-        int pick = nums[i] + f(i-2, nums, dp);
-        int notpick = 0 + f(i-1, nums, dp);
-
-        return dp[i] = max(pick, notpick);
-    }
-    // Memoization 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return f(n-1, nums, dp);   
+        int prev = nums[0];
+        int prev2 = 0;
+        // Tabulation + Space Optimized
+        for(int i=1; i<n; i++){
+            int take = nums[i];
+            if(i > 1) take += prev2;
+
+            int nottake = 0 + prev;
+
+            int curr = max(take, nottake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 };
