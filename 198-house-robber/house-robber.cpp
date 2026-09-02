6e6434845617901d95d69269c7ab8f2106f4a5d3
Vishesh Goel <visheshgoel0007@gmail.com>
2026-09-02 11:04:45 +0530

    Time: 1 ms (9.30%) | Memory: 10.6 MB (47.03%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int f(int i, vector<int>& nums, vector<int>& dp){
+        if(i == 0) return nums[i];
+        if(i < 0) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int pick = nums[i] + f(i-2, nums, dp);
+        int notpick = 0 + f(i-1, nums, dp);
+
+        return dp[i] = max(pick, notpick);
+    }
+    // Memoization 
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return f(n-1, nums, dp);   
+    }
+};
