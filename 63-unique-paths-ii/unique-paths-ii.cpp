dbb0ea93acbc1f7f8b50796ba1799056eab5240f
Vishesh Goel <visheshgoel0007@gmail.com>
2026-09-05 00:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.54%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
-        if(i<0 || j<0) return 0;
-        if(obstacleGrid[i][j] == 1) return 0;
-        if(i==0 && j==0) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = solve(i - 1, j, obstacleGrid, dp) + solve(i, j - 1, obstacleGrid, dp);
-    }
-    // Memoization Approach
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m - 1, n - 1, obstacleGrid, dp);
+        vector<int> dp(n, 0);
+        dp[0] = (obstacleGrid[0][0] == 0);
+        // Tabulation + Space Optimized
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                }
+                else if (j > 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
     }
 };
