0d9c05f9c0dead8cf1c6b9c669612d269842ba23
Vishesh Goel <visheshgoel0007@gmail.com>
2026-09-04 23:59:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (83.06%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& dp){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        return dp[i][j] = solve(i-1, j, dp) + solve(i, j-1, dp);
-    }
-    // Memoization Approach
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m-1, n-1, dp);
+        vector<int> dp(n, 1);
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+
+        return dp[n - 1];
     }
 };
